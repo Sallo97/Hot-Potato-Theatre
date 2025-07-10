351160f3af5babdced2d38ba47f140de663c3357
Sal <salerno.salvatore1997@gmail.com>
2025-07-10 16:38:58 +0200

    Added the ratio property in Potato.

--- src/main/kotlin/potato/Potato.kt
+++ src/main/kotlin/potato/Potato.kt
@@ -1,10 +1,13 @@
 package org.example.potato
 
 /**
- * Defines a hot potato object by three properties:
- * - lifetime: the number of turn in which the hot potato is still valid (i.e. the payoff is a gain)
- * - gain: the payoff for all players that take the hot potato and are not the last player. It is always positive
- * - loss: the payoff for the last player that takes the hot potato. It is always negative
+ * Defines a hot potato object.
+ * @property [lifetime] the number of turn in which the hot potato is still valid (i.e. the payoff is a gain).
+ * @property [gain] the payoff for all players that take the hot potato and are not the last player. It is always positive.
+ * @property [loss] the payoff for the last player that takes the hot potato. It is always negative.
+ * @property [ratio] the positive ratio between gain and loss (i.e. gain / (-loss)).
  * We interpret gain and loss value as dollars for sake of simplicity
  */
-data class Potato(val lifetime: Int, val gain:Int, val loss: Int)
+data class Potato(val lifetime: Int, val gain:Int, val loss: Int) {
+    val ratio : Double = gain.toDouble()/(-loss.toDouble())
+}
