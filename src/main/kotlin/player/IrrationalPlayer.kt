91d602718bb614b2eb0c298dfc903f3ae239483e
Sal <salerno.salvatore1997@gmail.com>
2025-07-11 11:09:09 +0200

    Fixed minor typos in the comments.

--- src/main/kotlin/player/IrrationalPlayer.kt
+++ src/main/kotlin/player/IrrationalPlayer.kt
@@ -6,7 +6,7 @@ import org.example.game.Game
  * Represents an irrational player in the SHPG, i.e. one which will always take the good if asked.
  *
  * @property [id] unique identifier for the player.
- * @property [owns_potato] describes as a boolean if the player is the current holder of the hot potato good or not.
+ * @property [ownsPotato] describes as a boolean if the player is the current holder of the hot potato good or not.
  * At the start a player hasn't the good.
  * @property [payoff] the payoff of the player. At the start it is always 0.
  * @constructor creates a player without the hot potato and with a payoff of 0.
@@ -16,7 +16,7 @@ class IrrationalPlayer (id: Int) : Player(id) {
     /**
      * Handles the decision logic of the irrational player in taking or not the good.
      *
-     * @return true, since a rational player will always accept a hot potato good.
+     * @return true, since a irrational player will always accept a hot potato good.
      */
     override fun decisionMaking(game: Game): Boolean {
         return true
