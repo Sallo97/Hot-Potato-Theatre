f0928f2e33e64a14933acd7435db1138dff34a94
Sal <salerno.salvatore1997@gmail.com>
2025-07-11 11:09:16 +0200

    Fixed minor typos in the comments.

--- src/main/kotlin/player/RationalPlayer.kt
+++ src/main/kotlin/player/RationalPlayer.kt
@@ -6,7 +6,7 @@ import org.example.game.Game
  * Represents a rational player in the SHPG.
  *
  * @property [id] unique identifier for the player.
- * @property [owns_potato] describes as a boolean if the player is the current holder of the hot potato good or not.
+ * @property [ownsPotato] describes as a boolean if the player is the current holder of the hot potato good or not.
  * At the start a player hasn't the good.
  * @property [payoff] the payoff of the player. At the start it is always 0.
  * @constructor creates a player without the hot potato and with a payoff of 0.
@@ -16,7 +16,7 @@ class RationalPlayer(id: Int) : Player(id) {
     /**
      * Handles the decision logic of the player behind either the acceptance or denying of the hot potato.
      *
-     * @return a rational player will never accept a hot potato good, since its is subgame perfect strategy.
+     * @return false, a rational player will never accept a hot potato good.
      */
     override fun decisionMaking (game: Game) : Boolean {
         return false
