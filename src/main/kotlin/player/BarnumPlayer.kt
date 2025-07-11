72e383b1de60aad5a5883d44124b34e933139932
Sal <salerno.salvatore1997@gmail.com>
2025-07-11 11:09:01 +0200

    Fixed minor typos in the comments.

--- src/main/kotlin/player/BarnumPlayer.kt
+++ src/main/kotlin/player/BarnumPlayer.kt
@@ -4,27 +4,28 @@ import org.example.game.Game
 import kotlin.math.pow
 
 /**
- * a Barnum player in the SHPG, i.e. a rational player aware that there is the *possibility* that irrational players
+ * A Barnum player in the SHPG, i.e. a rational player aware that there is the *possibility* that irrational players
  * are among the population.
  *
  * @property [prob] the probability that any other player in the population is a Barnum.
+ * It is represented as a value between 0 and 1 (included).
  * @property [id] unique identifier for the player.
- * @property [owns_potato] describes as a boolean if the player is the current holder of the hot potato good or not.
- * At the start a player hasn't the good.
- * @property [payoff] the payoff of the player. At the start it is always 0.
- * @constructor creates a player without the hot potato and with a payoff of 0.
+ * @property [ownsPotato] describes as a boolean if the player is the current holder of the hot potato good or not.
+ * @property [payoff] the payoff of the player.
+ * @constructor creates a player without the hot potato, with a [payoff] of 0 and with [prob] passed as argument.
  */
 class BarnumPlayer (id: Int, prob: Float) : Player(id) {
     val prob = if (prob <= 1.0 && prob >= 0) {
         prob
     } else {
-        error("prob should be a value between 0 and 1!")
+        error("prob = $prob must be a value between 0 and 1 (included)!")
     }
 
     /**
      * Handles the decision logic of the player behind either the acceptance or denying of the hot potato.
      * The decision of a Barnum player depends on the state of the game (the number of remaining players and
-     * if the hot portato is alive or not) and on the relationship between the ratio of gain and loss and the ratio between the probability that alters are Barnums or irrational.
+     * if the hot potato is alive or not) and on the relationship between the ratio of gain and loss and the
+     * ratio between the probability that alters are Barnums or irrational.
      *
      * @param [game] represents the current game state, which impacts the choice.
      * @return true if the player *chosen* to accept the good, false otherwise.
