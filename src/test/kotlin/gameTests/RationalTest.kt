e6a203b3928b43f4a9f917d1177bdbb51d61d5dc
Sal <salerno.salvatore1997@gmail.com>
2025-07-15 18:55:00 +0200

    Implemented basic tests on games composed of only Rational players.

---
+++ src/test/kotlin/gameTests/RationalTest.kt
@@ -0,0 +1,41 @@
+package gameTests
+
+import org.example.player.Player
+import org.example.player.RationalPlayer
+import org.example.potato.Potato
+import kotlin.test.Test
+
+class RationalGame : GameTest(){
+
+    @Test
+    fun testRational1() {
+        val potato = Potato(lifetime = 100u, gain = 100u, loss = 100u)
+        val players = mutableSetOf<Player>().apply {
+            for(i in 1..10) {
+                val player = RationalPlayer(i)
+               this.add(player)
+            }
+        }
+        doGameTest(potato,
+            players,
+            0u,
+            0,
+            0)
+
+    }
+
+    @Test
+    fun testRational2() {
+        val potato = Potato(lifetime = 0u, gain = 0u, loss = 1000u)
+        val players = mutableSetOf<Player>().apply {
+            val player = RationalPlayer(1)
+            this.add(player)
+        }
+        doGameTest(potato,
+            players,
+            0u,
+            0,
+            0)
+
+    }
+}
