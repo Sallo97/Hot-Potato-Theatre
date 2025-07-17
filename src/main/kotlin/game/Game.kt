a45854ac1a98962d6ea5a02415d5f065c06741a5
Sal <salerno.salvatore1997@gmail.com>
2025-07-17 18:24:07 +0200

    Now all game status is encapsulated withing an immutable GameStatus object. Additionally, the logic for finding a willing player has been moved from Player to here with the method findingWillingPlayer.

--- src/main/kotlin/game/Game.kt
+++ src/main/kotlin/game/Game.kt
@@ -1,6 +1,7 @@
 package org.example.game
 
 import org.example.coalition.Coalition
+import org.example.gameStatus.GameStatus
 import org.example.player.CoalitionalPlayer
 import org.example.player.Player
 import org.example.potato.Potato
@@ -10,21 +11,15 @@ import org.example.potato.Potato
  *
  * @property [potato] the hot potato good associated to the game.
  * @property [coalition] the active coalition of coalitional players in the game.
- * @property [activePopulation] the non-ordered subset of players which at the current turn never got the good.
- * @property [chain] the ordered set of players which at the current turn already got the good.
- * @property [turn] the current turn of the game.
- * @property [numOfPlayers] the total number of players in the game.
- * @property [totalPayoff] sum of the payoff of all players at the current turn.
+ * @property [status] keeps track of the state of the game.
  * @constructor creates a game with the given [potato] and population.
  */
 class Game (
     val potato: Potato,
-    val activePopulation: MutableSet<Player>) {
+    activePopulation: MutableSet<Player>) {
+
     val coalition: Coalition
-    private val chain: MutableList<Player> = mutableListOf()
-    var turn: UInt = 0u
-    val numOfPlayers: UInt = activePopulation.size.toUInt()
-    var totalPayoff = 0
+    private val status : GameStatus = GameStatus(activePopulation)
 
     init {
         val coalitionSetSize = activePopulation.filter { it is CoalitionalPlayer }.size.toUInt()
@@ -33,17 +28,17 @@ class Game (
 
     /**
      * Handles the game execution from start to end.
-     * @param [tryAll] if true for finding the starting player check over all the set for a willing player, otherwise
-     * will check only a single random player.
+     * @param [tryAll] if true for a willing player to take the potato check over all the set of available players,
+     * otherwise will check only a single random one.
      */
-    fun run(tryAll: Boolean = false) {
-        var foundNewHolder = findingStartingPlayer(tryAll)
+    fun run(tryAll: Boolean = true) {
+        var foundNewHolder = findingWillingPlayer(tryAll)
 
         while(foundNewHolder) {
             val holder = potato.currentHolder!!
             updateGame(holder)
 
-            foundNewHolder = holder.exchangePotato(this)
+            foundNewHolder = findingWillingPlayer(tryAll)
             givePayoff(holder, foundNewHolder)
         }
 
@@ -56,48 +51,60 @@ class Game (
      */
     fun getEndGameInfo () : String {
         val str = "Game ended with the following:\n" +
-                "- Potato's lifetime = ${potato.lifetime}\t turns = $turn\n" +
-                "- ${chainToString()}\n"
-        "- Total payoff = $totalPayoff\n"
+                "- Potato's lifetime = ${potato.lifetime}\t turns = ${status.turn}\n" +
+                "- ${status.chainToString()}\n"
+        "- Total payoff = ${status.totalPayoff}\n"
         return str
     }
 
-    /**
-     * @return the chain size
-     */
-    fun getChainSize() : Int {
-        return chain.size
-    }
-
     override fun toString(): String {
-        return "{ turn: $turn; numOfPlayers: $numOfPlayers; numOfRemainingPlayers: ${activePopulation.size} }"
+        return "{ turn: ${status.turn}; numOfPlayers: ${status.numOfPlayers}; " +
+                "numOfRemainingPlayers: ${status.activePopulation.size} }"
     }
 
     /**
      * @return true if the [potato] has not reached its end-of-life, false otherwise
      */
     fun isPotatoAlive () : Boolean{
-        if (potato.lifetime < turn) {
-            error("ERROR! Potato's lifetime (${potato.lifetime}) < current turn ($turn).\n The current turn  cannot " +
+        if (potato.lifetime < status.turn) {
+            error("ERROR! Potato's lifetime (${potato.lifetime}) < current turn ($status.turn).\n The current turn  cannot " +
                     "exceed the potato's lifetime ")
         }
-        return potato.lifetime > turn
+        return potato.lifetime > status.turn
+    }
+
+    /**
+     * @return the number of players available for getting the potato.
+     */
+    fun getNumOfAvailablePlayers(): Int {
+        return status.activePopulation.size
+    }
+
+    /**
+     * @return the current turn of the play.
+     */
+    fun getCurrentTurn() : Int {
+        return status.turn
+    }
+
+    /**
+     * @return the number of turns left before the end of the game.
+     */
+    fun getRemainingTurns() : Int {
+        val result = minOf(potato.lifetime - status.turn, getNumOfAvailablePlayers())
+        return result
     }
 
     /**
      * Updates the game status.
      *
-     * @param [p] the actor who decided to accept the good at the current [turn]
+     * @param [p] the actor who decided to accept the good at the current turn.
      */
     private fun updateGame (p: Player) {
-        p.let {
-            chain.add(it)
-            activePopulation.remove(it)
-            if (it is CoalitionalPlayer) {
-                coalition.addMember(it)
-            }
+        status.updateStatus(p)
+        if (p is CoalitionalPlayer) {
+            coalition.addMember(p)
         }
-        turn += 1u
     }
 
     /**
@@ -107,7 +114,7 @@ class Game (
      */
      private fun givePayoff(p:Player, isLastPlayer: Boolean = false) {
         val payoff = potato.getPayoff(isLastPlayer)
-        totalPayoff += payoff
+        status.totalPayoff += payoff
 
         if (p !is CoalitionalPlayer) {
             p.payoff += payoff
@@ -116,45 +123,33 @@ class Game (
         }
     }
 
-    private fun chainToString() :String {
-        var str = "Chain of players that partake in the game:"
-
-        if (chain.isEmpty()) {
-            str += "∅"
-        } else {
-            for (p in chain) {
-                str += "$p "
-            }
-        }
-
-        return str
-    }
-
     /**
-     * Select the first player to start the game. It is chosen at random within the set.
-     * @param [tryAll] if true check over all the set for a willing player, otherwise will check only a single random
+     * Search among the active population (i.e. those that did not already take the hot potato), the first one willing to
+     * take the good.
+     *
+     * @param [askAll] if true check over all the active set for a willing player, otherwise will check only a single random
      * player.
-     * @return true if a player was willing to accept the good, false otherwise.
+     * @return true if a new player to exchange the good was found, false otherwise.
      */
-    private fun findingStartingPlayer (tryAll: Boolean = false) : Boolean {
-        var found: Player? = null
-
-        if (tryAll) {
-            for (p in activePopulation) {
-                if(p.decideAcceptance(this)) {
-                    found = p
-                    break
-                }
-            }
-        } else {
-            val randomPlayer = activePopulation.random()
-
-            if(randomPlayer.decideAcceptance(this)) {
-                found = randomPlayer
-            }
+    private fun findingWillingPlayer(askAll: Boolean) : Boolean {
+        if (getRemainingTurns() <= 0) {
+            return false
         }
 
-        potato.currentHolder = found
-        return found != null
+        val candidates = if (askAll) {
+            status.activePopulation.asSequence()
+        } else {
+            sequenceOf(status.activePopulation.random())
+        }
+
+        val willingPlayer = candidates.firstOrNull { it.decideAcceptance(this) }
+
+        val result = if(willingPlayer != null) {
+            potato.currentHolder = willingPlayer
+            true
+        } else {
+            false
+        }
+        return result
     }
 }
