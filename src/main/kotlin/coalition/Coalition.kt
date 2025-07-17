6ac127c8d9b3e053d72ee8fa4f2eefb8c725ae91
Sal <salerno.salvatore1997@gmail.com>
2025-07-17 17:06:53 +0200

    Implemented the class Coalition, represent a coalition of coalitional players.

---
+++ src/main/kotlin/coalition/Coalition.kt
@@ -0,0 +1,44 @@
+package org.example.coalition
+
+import org.example.player.CoalitionalPlayer
+
+/**
+ * Implements a basic coalition between coalitional players, managing all the surrounding information.
+ *
+ * @property [totalCoalitionalPlayers] the total number of coalitional players in the game.
+ * @property [members]  set of coalitional players which are in the coalition.
+ * @property [totalPayoff] total payoff gained by the coalition thus far.
+ * @property [possibleMembers] the number of coalitional players not yet in the coalition.
+ */
+data class Coalition (
+    val totalCoalitionalPlayers: UInt) {
+
+    var totalPayoff : Double = 0.0
+    private val members: MutableSet<CoalitionalPlayer> = mutableSetOf()
+    var possibleMembers = totalCoalitionalPlayers
+
+    /**
+     * After game ended provide the payoff to all players which engaged in the coalition.
+     * The amount of payoff is equal to all members of the coalition.
+     */
+    fun splitTotalPayoff() {
+        val payoff: Double = totalPayoff / members.size
+        for (p in members) {
+            p.payoff += payoff
+        }
+        totalPayoff = 0.0
+    }
+
+     fun addMember(p: CoalitionalPlayer) {
+        members.add(p)
+        possibleMembers--
+    }
+
+    /**
+     * @return the number of players in the coalition.
+     */
+    fun size(): Int {
+        return members.size
+    }
+
+}
