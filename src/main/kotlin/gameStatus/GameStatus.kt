3fa6bab77bd8617a0f827881d874ce3023a6acee
Sal <salerno.salvatore1997@gmail.com>
2025-07-17 18:22:41 +0200

    Better comment and moved the method chanToString here.

--- src/main/kotlin/gameStatus/GameStatus.kt
+++ src/main/kotlin/gameStatus/GameStatus.kt
@@ -4,16 +4,20 @@ import org.example.player.Player
 
 /**
  * Contains useful information regarding the status of a SHPG
- *
+ * @property [activePopulation] the non-ordered subset of players which at the current turn never got the good.
+ * @property [chain] the ordered set of players which at the current turn already got the good.
+ * @property [turn] the current turn of the game.
+ * @property [numOfPlayers] the total number of players in the game.
+ * @property [totalPayoff] sum of the payoff of all players at the current turn.
  *
  */
 data class GameStatus(
     val activePopulation: MutableSet<Player>,
 ) {
-    private val chain: MutableList<Player> = mutableListOf()
+    val chain: MutableList<Player> = mutableListOf()
     var turn: Int = 0
     val numOfPlayers: Int = activePopulation.size
-    var totalPayoff = 0
+    var totalPayoff: Int = 0
 
     /**
      * updates the game status after a turn has been played.
@@ -22,5 +26,19 @@ data class GameStatus(
         chain.add(p)
         activePopulation.remove(p)
         turn += 1
+    }
+
+    fun chainToString() :String {
+        var str = "Chain of players that partake in the game:"
+
+        if (chain.isEmpty()) {
+            str += "∅"
+        } else {
+            for (p in chain) {
+                str += "$p "
+            }
+        }
+
+        return str
     }
 }
