8477d468df7960421069192ea60bde0b26f6789a
Sal <salerno.salvatore1997@gmail.com>
2025-07-20 10:54:13 +0200

    Redesigned acceptance logic.

--- src/main/kotlin/backend/player/BenthamitePlayer.kt
+++ src/main/kotlin/backend/player/BenthamitePlayer.kt
@@ -1,21 +1,20 @@
 package org.example.backend.player
 
 import org.example.backend.game.Game
-import kotlin.math.pow
 
 /**
  * A Benthamite player in the SHPG, i.e. an irrational player not concerned with their own payoff, rather it is focused
  * on maximizing the total payoff of the game. Still, if during a decision, it is aware that other players could take the
  * risk, then it could reject the potato.
  *
- * @property [alterAcceptBelief] the belief of the current player that an alter will accept the potato at the current
- * turn. Is a value in [0,1]. Default value is 1/2.
+ * @property [gainWeight] how much weight has the acceptance of the hot potato.
+ * @property [lossWeight] how much weight has the deny of the hot potato
  * @property [payoff] the payoff of the player.
- * @constructor creates a player without the hot potato, with a [payoff] of 0 and with [alterAcceptBelief] passed as argument.
+ * @constructor creates a player without the hot potato, with a [payoff] of 0 and with [gainWeight] passed as argument.
  */
-class BenthamitePlayer(id: Int, val alterAcceptBelief: Double = 0.5) : Player(id){
+class BenthamitePlayer(id: Int, val gainWeight: Double = 0.5, val lossWeight: Double = 0.5) : Player(id){
     init {
-        require(alterAcceptBelief in 0.0..1.0)
+        require(gainWeight in 0.0..1.0)
     }
 
     /**
@@ -28,18 +27,16 @@ class BenthamitePlayer(id: Int, val alterAcceptBelief: Double = 0.5) : Player(id
     override fun decideAcceptance(game: Game): Boolean {
         val potato = game.potato
         val remainingTurns = game.getRemainingTurnsExceptCurrent()
+        val remainingPlayers = game.getNumOfAvailablePlayers()
 
-        val probAnotherAlterWillAccept = alterAcceptBelief.pow(remainingTurns)
+        val groupBenefit = gainWeight * (potato.gain * remainingTurns - potato.loss)
+        val diffusionResponsibility = lossWeight * (1/remainingPlayers)
 
-        val gainWeight = (potato.gain.toDouble() * remainingTurns.toDouble()
-                * (1 - probAnotherAlterWillAccept)) - potato.loss.toDouble()
-        val lossWeight = potato.loss.toDouble()
-
-        val decision = gainWeight > lossWeight
+        val decision = groupBenefit > diffusionResponsibility
         return decision
     }
 
     override fun toString(): String {
-        return "{id: $id; payoff: $payoff; alterAcceptBelief: $alterAcceptBelief}"
+        return "{id: $id; payoff: $payoff; alterAcceptBelief: $gainWeight}"
     }
 }
