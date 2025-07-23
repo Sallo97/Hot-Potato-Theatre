f12d1bc08b22d89ef5137825b2f7b7fdf8b5f830
Sal <salerno.salvatore1997@gmail.com>
2025-07-23 23:57:08 +0200

    Removed Compose Multidesktop from gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     kotlin("jvm") version "2.1.10"
-    id("org.jetbrains.compose") version "1.8.2"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
 }
 
 group = "org.example"
@@ -9,18 +7,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
 }
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation(compose.desktop.currentOs)
-    implementation(compose.ui)
-    implementation(compose.uiTooling)
-    implementation(compose.foundation)
-    implementation(compose.material3)
-    implementation(compose.runtime)
 }
 
 tasks.test {
@@ -28,10 +19,4 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
-}
-
-compose.desktop {
-    application {
-        mainClass = "MainKt" // Replace with your main class
-    }
 }
